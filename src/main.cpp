b22971e72d68d9ab314793ca9cdd1eed471c976f
Rafa-Correia <rafaelazevedocorreia04@gmail.com>
2025-07-25 22:43:37 +0100

    Initial project structure done. Very simple! Change later!

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * Hey! This is the main source file for the engine!
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello world!" << std::endl;
+
+    return 0;
+}
