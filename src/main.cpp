44a30abe6f6687aa1fc74ae1c6ae7ef880621685
Rafa-Correia <rafaelazevedocorreia04@gmail.com>
2025-07-31 23:23:05 +0100

    Now using shaders! (super very much super ultra mega basic like super basic)

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 /**
  * Hey! This is the main source file for the engine!
- * 
+ *
  * This whole project is just my way of testing my graphics programming
  * capabilities. I want to make a 3D engine from the ground up, like I did
  * during my semester with the Graphics Computing course, except this time
  * I would not be using the OpenGL Fixed Function Pipeline, and instead I'd
- * like to use shaders! That way I'd also get familiar with GLSL. 
- * 
- * I also want to try and build a 3D scene, both the format and the parsing.
- * 
+ * like to use shaders! That way I'd also get familiar with GLSL.
+ *
+ * I also want to try and build a 3D scene, both the file format and the parsing.
+ * Above that, I'd like to be able to parse at least .obj files, if not other
+ * formats in the future.
+ *
  * We'll see how this goes.
  */
 
@@ -25,30 +27,113 @@
 
 #endif
 
-float angle = 0;
+#define LOG_LENGTH (GLsizei)512
 
-void render() {
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(0.0f, 0.9f);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex2f(-0.9f, -0.9f);
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex2f(0.9f, -0.9f);
-    glEnd();
+GLuint VAO, VBO;
+GLuint shader_program_g;
+
+/**
+ * So now like, you'd load a config file with paths to all shaders and stuff.
+ * I don't have that setup yet. So like, the shader source (very basic) will
+ * stay here.
+ */
+const char vertex_shader_glsl[] = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+out vec4 vertexColor;
+void main() {
+    gl_Position = vec4(aPos, 1.0);
+    vertexColor = vec4(0.5, 0.0, 0.0, 1.0);
+})";
+
+const char fragment_shader_glsl[] = R"(
+#version 330 core
+in vec4 vertexColor;
+out vec4 FragColor;
+void main() {
+    FragColor = vertexColor;
+})";
+
+const float triangle_vertices[] = {
+    -0.9f, -0.9f, 0.0f,
+    -0.9f, 0.9f, 0.0f,
+    0.0f, 0.9f, 0.0f};
+
+GLuint
+compile_shader(GLenum shader_type, const char *shader_source)
+{
+    GLuint shader = glCreateShader(shader_type);
+
+    glShaderSource(shader, 1, &shader_source, nullptr);
+
+    glCompileShader(shader);
+
+    // check if compilation success
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char shader_log[LOG_LENGTH];
+        glGetShaderInfoLog(shader, LOG_LENGTH, nullptr, shader_log);
+        std::cout << "Failed to compile shader: " << shader_log << std::endl;
+    }
+
+    return shader;
+}
+
+/**
+ * For now, only supports vertex and fragment!
+ */
+GLuint create_shader_program(const char *vertex_source, const char *fragment_source)
+{
+    GLuint vertex_shader = compile_shader(GL_VERTEX_SHADER, vertex_source);
+    GLuint fragment_shader = compile_shader(GL_FRAGMENT_SHADER, fragment_source);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+
+    glLinkProgram(shader_program);
+
+    // check if program link success
+    GLint success;
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+
+    if (!success)
+    {
+        char program_log[LOG_LENGTH];
+        glGetProgramInfoLog(shader_program, LOG_LENGTH, nullptr, program_log);
+
+        std::cout << "Failed to link shader program: " << program_log << std::endl;
+    }
+
+    return shader_program;
+}
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader_program_g);
+
+    glBindVertexArray(VAO);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glutSwapBuffers();
     return;
 }
 
-void idle() {
+void idle()
+{
     return;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     /**
      * For now, I will use hardcoded values.
-     * 
+     *
      *   * CHANGE LATER TO ACCEPT A SCENE FILE AS AN ENTRY POINT! *
      *   * MAYBE A CONFIGURATION FILE INSTEAD OF A SCENE, WE'LL SEE *
      */
@@ -64,7 +149,24 @@ int main(int argc, char **argv) {
 
     glutDisplayFunc(render);
     glutIdleFunc(idle);
-    
+
+#ifndef __APPLE__
+    glewInit();
+#endif
+
+    shader_program_g = create_shader_program(vertex_shader_glsl, fragment_shader_glsl);
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
     glutMainLoop();
 
     return 0;
